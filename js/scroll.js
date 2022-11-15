0096676500774e94889309d8092b0add1a5ec09e
Matias Altamirano <matytiyu4@gmail.com>
2022-11-14 21:56:56 -0300

    d

---
+++ js/scroll.js
@@ -0,0 +1,55 @@
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+function reveal_left() {
+    var reveals = document.querySelectorAll(".reveal-left");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 100;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+
+function reveal_right() {
+    var reveals = document.querySelectorAll(".reveal-right");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 100;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+
+
+
+
+  
+window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", reveal_left);
+window.addEventListener("scroll", reveal_right);
+
